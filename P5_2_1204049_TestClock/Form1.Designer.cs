6dc8a82c5ab649770298745093ce2b184f088534
Zian Asti Dwiyanti <ziandwiasti23@gmail.com>
2021-12-04 22:31:56 +0700

    Upload

---
+++ P5_2_1204049_TestClock/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace P5_2_1204049_TestClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.digitalClock1 = new P5_2_1204049.DigitalClock();
+            this.UniversalTimeLabel = new System.Windows.Forms.Label();
+            this.StartStopButton = new System.Windows.Forms.Button();
+            this.LocalTimeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // digitalClock1
+            // 
+            this.digitalClock1.LocalTimeLabel_BackColor = System.Drawing.SystemColors.ControlLight;
+            this.digitalClock1.Location = new System.Drawing.Point(164, 46);
+            this.digitalClock1.Name = "digitalClock1";
+            this.digitalClock1.Size = new System.Drawing.Size(141, 68);
+            this.digitalClock1.TabIndex = 0;
+            this.digitalClock1.Timer1_Enabled = true;
+            this.digitalClock1.RaiseTimer1_Tick += new System.EventHandler(this.digitalClock1_RaiseTimer1_Tick);
+            // 
+            // UniversalTimeLabel
+            // 
+            this.UniversalTimeLabel.AutoSize = true;
+            this.UniversalTimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UniversalTimeLabel.Location = new System.Drawing.Point(8, 116);
+            this.UniversalTimeLabel.Name = "UniversalTimeLabel";
+            this.UniversalTimeLabel.Size = new System.Drawing.Size(132, 20);
+            this.UniversalTimeLabel.TabIndex = 1;
+            this.UniversalTimeLabel.Text = "Universal Label";
+            // 
+            // StartStopButton
+            // 
+            this.StartStopButton.Location = new System.Drawing.Point(249, 215);
+            this.StartStopButton.Name = "StartStopButton";
+            this.StartStopButton.Size = new System.Drawing.Size(124, 31);
+            this.StartStopButton.TabIndex = 2;
+            this.StartStopButton.Text = "Start/Stop";
+            this.StartStopButton.UseVisualStyleBackColor = true;
+            this.StartStopButton.Click += new System.EventHandler(this.StartStopButton_Click);
+            // 
+            // LocalTimeLabel
+            // 
+            this.LocalTimeLabel.AutoSize = true;
+            this.LocalTimeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LocalTimeLabel.Location = new System.Drawing.Point(12, 68);
+            this.LocalTimeLabel.Name = "LocalTimeLabel";
+            this.LocalTimeLabel.Size = new System.Drawing.Size(95, 20);
+            this.LocalTimeLabel.TabIndex = 3;
+            this.LocalTimeLabel.Text = "Local Time";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 270);
+            this.Controls.Add(this.LocalTimeLabel);
+            this.Controls.Add(this.StartStopButton);
+            this.Controls.Add(this.UniversalTimeLabel);
+            this.Controls.Add(this.digitalClock1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private P5_2_1204049.DigitalClock digitalClock1;
+        private System.Windows.Forms.Label UniversalTimeLabel;
+        private System.Windows.Forms.Button StartStopButton;
+        private System.Windows.Forms.Label LocalTimeLabel;
+    }
+}
+
